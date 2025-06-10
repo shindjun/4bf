764c7596bada6e199a27d59820bd971f41903327
shindjun <shindjun@gmail.com>
2025-06-10 13:42:47 +0900

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="p-6 text-center text-xl font-bold text-blue-600">
+      안녕하세요! 고로 계산기입니다 🔥
+    </div>
+  );
+}
+
+export default App;
